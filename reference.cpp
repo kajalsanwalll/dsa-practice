7f92326ed06d34ead2b476be52ba946604793b12
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-23 21:30:02 +0530

    reference

---
+++ reference.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+int a = 10;
+int &r = a; // referencing or giving nickname to a 
+
+cout << a << endl;
+cout << r << endl;
+
+}
