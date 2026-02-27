51ee3fa924b71f4e519be05d6dd8322dd48c0179
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-27 16:32:47 +0530

    pass by address for swap

--- parameterPassing.cpp
+++ parameterPassing.cpp
@@ -3,10 +3,22 @@
 
 using namespace std;
 
-
+// pass by address to swap
+    void swap(int *x, int *y){
+        int temp;
+        temp = *x;
+        *x=*y;
+        *y= temp;
+    }
 
 
 int main(){
 
+    //pass by reference to swap
+    int a,b;
+    a=10;
+    b=20;
+    swap(&a,&b);
+    cout << a << "," << b << endl;
     
 }
