762abf3aa2bf21568dba602b06d0a1f2ce80651e
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-27 16:47:31 +0530

    pass by reference

--- parameterPassing.cpp
+++ parameterPassing.cpp
@@ -11,14 +11,29 @@ using namespace std;
         *y= temp;
     }
 
+// pass by reference to swap
+    void swap1(int &x1, int &y1){
+        int temp;
+        temp = x1;
+        x1=y1;
+        y1= temp;
+    }
+
 
 int main(){
 
-    //pass by reference to swap
+    //pass by reference to swap (c++ allows it by reference)
     int a,b;
     a=10;
     b=20;
     swap(&a,&b);
     cout << a << "," << b << endl;
+
+    int a1,b1;
+    a1=8;
+    b1=9;
+    swap1(a1,b1);
+    cout << a1 << "," << b1 << endl;
+
     
 }
