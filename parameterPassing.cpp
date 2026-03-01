412eb28a4df9030a2886a04727670fb0d5602439
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-01 13:11:07 +0530

    func returning array

--- parameterPassing.cpp
+++ parameterPassing.cpp
@@ -19,14 +19,23 @@ using namespace std;
         y1= temp;
         
     }
-
+    
 // array as a parameter
-
 void fun(int A[], int n){
     int i;
+
+    A[0] = 23;
      
     for(i=0; i<n; i++)
     cout << "array as a parameter: " << A[i] << endl;
+}
+
+//function returning an array
+int * func(int n){
+    int *p;
+    p = (int *)malloc(n*sizeof(int));
+    return p;
+
 }
 
 
@@ -39,15 +48,24 @@ int main(){
     swap(&a,&b);
     cout << "pass by address swap:" << a << "," << b << endl;
 
-     //pass by reference to swap (c++ allows it by reference)
+    //pass by reference to swap (c++ allows it by reference)
     int a1,b1;
     a1=8;
     b1=9;
     swap1(a1,b1);
     cout << "pass by reference swap:" << a1 << "," << b1 << endl;
 
-    int A[5] = {2,4,6,8,10};
-    fun(A,5);
+    //array as parameter
+    int A[] = {2,4,6,8,10};
+    int n = 5;
+    fun(A,n);
 
+    //function returning an array
+    int *A1;
+    int j;
+    A1= func(5);
+
+    for(j=0; j<5; j++)
+    cout << "func returning an array: " << A1[j] << endl;
 
 }
