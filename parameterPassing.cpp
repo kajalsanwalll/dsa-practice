168b9ea45a60fecc76e9d9822d0ab2fe69f0811d
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-01 12:20:50 +0530

    array as a parameter

--- parameterPassing.cpp
+++ parameterPassing.cpp
@@ -20,6 +20,15 @@ using namespace std;
         
     }
 
+// array as a parameter
+
+void fun(int A[], int n){
+    int i;
+     
+    for(i=0; i<n; i++)
+    cout << "array as a parameter: " << A[i] << endl;
+}
+
 
 int main(){
 
@@ -37,5 +46,8 @@ int main(){
     swap1(a1,b1);
     cout << "pass by reference swap:" << a1 << "," << b1 << endl;
 
-    
+    int A[5] = {2,4,6,8,10};
+    fun(A,5);
+
+
 }
