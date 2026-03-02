75fcc30c98a24e990f099b020573b6ddebab38a4
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-02 16:43:53 +0530

    monolithic style of coding

---
+++ monolithic.cpp
@@ -0,0 +1,19 @@
+// all code in int main = monolithic style of coding
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+    int length=0;
+    int breadth=0;
+
+    cout<<"enter length and breadth: " << endl;
+    cin >> length >> breadth;
+
+
+    return 0;
+
+}
