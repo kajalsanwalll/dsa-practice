abb4e6f27430f361cddce0ff21e81be32f96ff89
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-03 19:35:43 +0530

    class oop

--- classesAndConstructors.cpp
+++ classesAndConstructors.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 class Rectangle
 {
   private:
@@ -6,33 +8,49 @@ class Rectangle
 
   public:
     //constructor function = to initialize 
-    Rectangle (int l, int b){
-        length =l;
-        breadth = b;
-    } 
+    Rectangle (int l, int b);
     
-    //area function
-    int area(){
-        return length*breadth;
-    }
+    //area function. known as facilitators = functions that work
+    int area();
+    int perimeter();
 
     //get length
-    int getLength()
+    int getLength()  //accessor
     {
         return length;
     }
     //change length function
-    void setLength(int l){
+    void setLength(int l) // mutator
+    {
         length=l;
     }
 
    // ~Rectangle(); //destructor
 };
 
+
+//now implementing these class functions using scoap resolution
+Rectangle ::Rectangle(int l, int b){
+    length =l;
+    breadth=b;
+}
+
+int Rectangle::area(){
+    return length*breadth;
+}
+
+int Rectangle::perimeter(){
+    return 2*(length+breadth);
+}
+
+//Rectangle::~Rectangle(){}
+
 int main(){
     Rectangle r(10,5);
 
-    r.area();
+    cout << "area: " << r.area() <<endl;
+    cout << "perimeter: " << r.perimeter()<<endl;
     r.setLength(7);
+    cout << "length: " << r.getLength()<<endl;
 
 }
