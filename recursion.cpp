66b4b00bf3cfba01dca6c5090deff8854c9a72b7
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-14 15:09:14 +0530

    recursion

---
+++ recursion.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void fun(int n){
+    if (n>0){
+        printf("%d\n",n);
+        fun(n-1);
+    }
+}
+
+int main(){
+    int x=3;
+    
+    fun(x);
+
+    return 0;
+}
