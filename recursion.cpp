97204bc84eff3d50569f3de2572816ab85ebf9bc
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-14 15:19:19 +0530

    fun2 added

--- recursion.cpp
+++ recursion.cpp
@@ -10,10 +10,20 @@ void fun(int n){
     }
 }
 
+void fun2(int n){
+    if (n>0){
+
+        fun2(n-1);
+        printf("%d\n",n);
+
+    }
+}
+
 int main(){
     int x=3;
     
     fun(x);
+    fun2(x);
 
     return 0;
 }
