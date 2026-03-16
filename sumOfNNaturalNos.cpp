337b7c408b361a918d60167958f6afa9db98fd0b
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-16 20:27:23 +0530

    sum using for loop

--- sumOfNNaturalNos.cpp
+++ sumOfNNaturalNos.cpp
@@ -18,6 +18,16 @@ int sum(int n){
     return n*(n+1)/2;
 }
 
+//sum of n natural nos. using loop
+int SUM(int n){
+    int i,s = 0;
+
+    for(i=1;i<=n; i++){
+        s=s+i;
+    }
+    return s;
+}
+
 int main(){
     int s;
     s = Sum(15);
@@ -26,6 +36,10 @@ int main(){
     int S;
     S = sum(15);
     cout << "sum using formula: " << S << endl;
+
+    int su;
+    su = SUM(15);
+    cout << "sum using loop: " << su << endl;
                         
 }
 
