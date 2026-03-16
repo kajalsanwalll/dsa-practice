2319eca7970da7a13ee714f43950b0c588cf1717
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-16 20:02:19 +0530

    sum of n natural nos using recursion

--- typesofRecursion.cpp
+++ typesofRecursion.cpp
@@ -50,6 +50,16 @@ int funN(int n){
     }
 }
 
+//sum of n natural numbers using recursion.
+int Sum(int n){
+
+    if(n==0){
+        return 0;
+    }else{
+        return Sum(n-1)+n;
+    }
+}
+
 int main(){
 
     fun(3);
@@ -60,6 +70,10 @@ int main(){
 
     r = funN(95);
     cout << "value of final ans of nested: " << r << endl;
+
+    int s;
+    s = Sum(15);
+    cout << "sum of 15 natural nos is: " << s << endl;
 
     return 0;
 }
