dd39cf284f8f9c99fa565d276140a097199d51ec
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-16 21:49:46 +0530

    exponent using recursion

---
+++ exponentsRecursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//power using recursion
+int pow(int m, int n){
+   if(n==0){
+    return 1;
+   }else{
+    return pow(m, n-1)*m;
+   }
+}
+
+int main(){
+
+    int p;
+    p = pow(2,5);
+    cout << "power of 2^5 is: " << p << endl;
+}
