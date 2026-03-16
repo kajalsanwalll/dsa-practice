63537198290f03da5c02f241c7347800e66cbd43
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-16 15:41:37 +0530

    indirect recursion

--- typesofRecursion.cpp
+++ typesofRecursion.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+//head recursion = function calls itself at the starting of the program
+//tail recursion = function calls itself at the returning time/ end of program
 // tree recursion - functions calls itself more than once
 void fun(int n){
     
@@ -15,10 +18,31 @@ void fun(int n){
     }
 }
 
+//indirect recursion = when funA calls funB and funB calls funC which then calls back funA.
+void funB(int n);
+void funA(int n){
+    
+    if(n>0){
+        printf("%d\n",n);
+        funB(n-1);
+    }
+
+}
+
+void funB(int n){
+
+    if(n>1){
+        printf("%d\n", n);
+        funA(n/2);
+    }
+
+}
+
 int main(){
 
     fun(3);
 
+    funA(20);
 
     return 0;
 }
