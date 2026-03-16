37338a93e956e6d088b50b8afeb02a801813f437
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-16 11:26:09 +0530

    types of recursion- tree recursion, tail, head recursion

---
+++ typesofRecursion.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+// tree recursion - functions calls itself more than once
+void fun(int n){
+    
+    if(n>0){
+
+
+
+    }
+}
+
+int main(){
+
+    return 0;
+}
