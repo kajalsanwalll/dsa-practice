8ee4023ad0ab3f2cc0668382a17f2a77d2c4e8ba
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-16 20:43:13 +0530

    factorial using loop

--- factorialRecursion.cpp
+++ factorialRecursion.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//factorial using recursion
 int factorial(int n){
     if(n==0){
         return 1;
@@ -9,9 +10,23 @@ int factorial(int n){
     }
 }
 
+//factorial using loop
+int fact(int n){
+    int i,s = 1;
+
+    for(i=1;i<=n; i++){
+        s=s*i;
+    }
+    return s;
+}
+
 int main(){
 
     int f;
     f = factorial(5);
     cout << "factorial of 5: " << f << endl; 
+
+    int su;
+    su = fact(5);
+    cout << "factorial using loop: " << su << endl;
 }
