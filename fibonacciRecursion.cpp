aae4994bd382fe9400428b4c26de6aa34ef0616b
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-21 17:48:28 +0530

    memoized function

--- fibonacciRecursion.cpp
+++ fibonacciRecursion.cpp
@@ -28,8 +28,30 @@ int fib(int n){
     return s;
 }
 
+//using MEMOIZATION = to reduce no of calls and order to O(n) by storing result in array
+int F[10];
+int mfib(int n){
+    
+    if(n<=1){
+        F[n]=n;
+        return n;
+    }
+    else{
+
+        if(F[n-2] == -1){
+            F[n-2]= mfib(n-2);
+        }
+        if(F[n-1] == -1){
+            F[n-1]= mfib(n-1);
+        }
+
+        return F[n-2] + F[n-1];
+    }
+}
+
 int main(){
     
     cout << "value of 7th term using recursion: " << fibo(7) << endl;
     cout << "value of 7th term using iteration: " << fib(7) << endl;
+    cout << "value of 7th term using memoized function: " << fib(7) << endl;
 }
