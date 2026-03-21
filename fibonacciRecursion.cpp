2d40ad49b425fb7ca32e957f608e2c88cd2421e5
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-21 17:14:47 +0530

    fibonacci using recursion and iteration

--- fibonacciRecursion.cpp
+++ fibonacciRecursion.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int fibo(int n, int m){
+//recursion
+int fibo(int n){
+  
+    if(n<=1){
+        return n;
+    }
+    return fibo(n-2)+ fibo(n-1);
+}
 
+//iteration
+int fib(int n){
+
+    int t0=0, t1=1, s , i;
+
+    if(n<=1){
+        return n;
+    }
+
+    for(i=2; i<=n; i++){
+        s = t0+t1;
+        t0=t1;
+        t1=s;
+    }
+
+    return s;
 }
 
 int main(){
     
+    cout << "value of 7th term using recursion: " << fibo(7) << endl;
+    cout << "value of 7th term using iteration: " << fib(7) << endl;
 }
