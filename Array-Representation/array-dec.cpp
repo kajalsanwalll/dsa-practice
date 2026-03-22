3c915322ddd0e544dce80d68e11bcd4cf56867e8
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-22 14:31:25 +0530

    array declaration and initialisation

---
+++ Array-Representation/array-dec.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int A0[5]; //only declaration
+
+    int A1[5] = {2,3,4,5,6}; //declaration and initialisation
+
+    int A2[] = {2,4,6}; //declaration and initialisation without giving number of indices
+
+    int A3[5] = {0}; // all zeros initialised
+
+
+}
