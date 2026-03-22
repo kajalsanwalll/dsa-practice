a8a4a47542a15002e48bd182b4eb19d745fefadd
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-22 11:52:42 +0530

    hanoi tower with recursion

--- toh.cpp
+++ toh.cpp
@@ -7,7 +7,7 @@ void TOH(int n, int A, int B, int C){
     if(n>0){
 
         TOH(n-1, A,C,B);
-        cout << "from" << A << "to" << C << endl;
+        cout << "from " << A << " to " << C << endl;
         TOH(n-1,B,A,C);
     }
 }
