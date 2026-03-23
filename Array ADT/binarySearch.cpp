4638fd2bab8c14df6df1495aabc10a6fab7ca7e8
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-23 13:17:16 +0530

    binary search using recursion and iteration in array

--- Array ADT/binarySearch.cpp
+++ Array ADT/binarySearch.cpp
@@ -21,6 +21,7 @@ void Display(struct Array arr){
 
 }
 
+//iterative function for binary search
 int BinarySearch(struct Array arr, int key){
 
     int l,i,h,mid;
@@ -47,12 +48,40 @@ int BinarySearch(struct Array arr, int key){
     return -1;
 }
 
+//recursive function for binary search
+int RBinarySearch(int a[],int l, int h, int key){
+
+    int mid;
+
+    if(l<=h){
+
+        mid = (l+h)/2;
+
+        if(key == a[mid]){
+            return mid;
+        }
+        else if(key < a[mid]){
+            return RBinarySearch( a, l, mid-1, key);
+        }
+        else{
+            return RBinarySearch(a, mid+1 , h, key);
+        }
+
+    }
+
+    return -1;
+}
+
 int main(){
 
     struct Array arr = {{1,2,3,4,5,6,7,8,10},10,9};
 
     printf("%d\n", BinarySearch(arr,7));
     Display(arr);
+
+    printf("\n%d\n", RBinarySearch(arr.A,0,arr.length,8));
+    Display(arr);
+
 
     return 0;
 }
