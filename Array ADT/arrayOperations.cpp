af2b0f7efd711b24d0a875a1b3a928ccebfbd81d
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-23 11:17:21 +0530

    append array

--- Array ADT/arrayOperations.cpp
+++ Array ADT/arrayOperations.cpp
@@ -4,40 +4,49 @@
 using namespace std;
 
 //Array structure in heap
-struct Array
+struct Array1
 {
-    int *A;
+    int *B;
     int size;
     int length;
 };
 
 //array structure in stack
-struct Array1{
+struct Array{
 
-    int B[20];
+    int A[10];
     int size;
     int length;
 };
 
 //for heap display
-void Display(struct Array arr){
+void Display(struct Array1 arr){
 
     int i;
     printf("\nelements of this array are: \n");
 
     for(i=0;i<arr.length; i++){
-        printf("%d ", arr.A[i]);
+        printf("%d ", arr.B[i]);
     }
 }
 
 //for stack display
-void Display(struct Array1 array){
+void Display(struct Array array){
 
     int i;
     printf("\nelements of stack array are: \n");
 
     for(i=0;i<array.length; i++){
-        printf("%d ", array.B[i]);
+        printf("%d ", array.A[i]);
+    }
+}
+
+//append function for stack array
+void Append(struct Array *array, int x) // *array because call by address and int x= value to be added
+{
+
+    if(array->length < array->size){
+        array->A[array->length++] = x;
     }
 }
 
@@ -45,13 +54,13 @@ int main(){
 
 
     // array in heap memory
-    struct Array arr;
+    struct Array1 arr;
     int i,n;
 
     printf("Enter size of array: ");
     scanf("%d", &arr.size);
 
-    arr.A= new int[arr.size];
+    arr.B= new int[arr.size];
     arr.length=0;
 
     printf("Enter number of total elements: ");
@@ -61,7 +70,7 @@ int main(){
 
     //read all elements and store them in an array arr from start to n
     for(i=0; i<n; i++){
-        scanf("%d", &arr.A[i]);
+        scanf("%d", &arr.B[i]);
     }
 
     arr.length=n;
@@ -69,9 +78,11 @@ int main(){
     Display(arr);
 
     // array in stack memory
-    struct Array1 array={{2,3,4,5}, 20, 4};
+    struct Array array={{2,3,4,5}, 10, 4};
 
+    Append(&array, 10);
     Display(array);
+
 
     return 0;
 }
