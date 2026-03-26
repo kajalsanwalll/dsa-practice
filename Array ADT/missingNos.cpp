2f3cc883d491cd7a89c1b68f1b52be46c60b9d10
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-26 14:45:58 +0530

    find missing no from sorted array of n natural nos but not starting from 1

--- Array ADT/missingNos.cpp
+++ Array ADT/missingNos.cpp
@@ -19,14 +19,35 @@ int missing(struct Array arr, int sum, int n){
     s= n*(n+1)/2;
     return s-sum;
 
-    cout << "missing number is: " << s-sum << endl;
+}
+
+//finding missing number in a sorted array of n natural numbers NOT starting from 1
+int Missing(struct Array arr1,int l, int h, int n){
+
+    int i;
+    int diff = l-0;
+
+    for( i=0;i<n;i++){
+
+        if(arr1.A[i]-i != diff){
+            
+            cout << "missing number is: " << i+diff << endl;
+            break;
+        }
+        
+    }
+
+    return i+diff;
 }
 
 
 int main(){
 
     struct Array arr={{1,2,3,4,5,6,8,9,10},11,9};
-    printf("%d",missing(arr,0,10));
+    printf("%d\n",missing(arr,0,10));
+
+    struct Array arr1={{6,7,8,9,10,12,13,14,15,16,17},11,11};
+    printf("%d",Missing(arr1,6,15,11));
 
     return 0;
 }
