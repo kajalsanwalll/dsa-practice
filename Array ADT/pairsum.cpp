368c82c4d6a183549f8f1fa23447667f45cd10c1
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-27 13:56:43 +0530

    pair sum unsorted array using hash table

--- Array ADT/pairsum.cpp
+++ Array ADT/pairsum.cpp
@@ -27,10 +27,26 @@ int pairsum(struct Array arr, int n, int k){
     return 0;
 }
 
+// UNSORTED ARRAY pairsum using hash table
+int hashPairSum(struct Array arr, int n, int k){
+
+    int i;
+    for(i=0;i<n;i++){
+
+        if(k - arr.A[i] >= 0 && arr.H[k - arr.A[i]] != 0){
+            cout << arr.A[i] << "+" << k-arr.A[i] << "=" << k << endl;
+        }
+        arr.H[arr.A[i]]++;
+    }
+
+    return 0;
+}
+
 int main(){
 
     struct Array arr={{6,3,8,10,16,7,5,2,9,14},10,10};
-    pairsum(arr, 10, 10);
+    //pairsum(arr, 10, 10);
+    hashPairSum(arr,10,10);
 
     return 0;
 
