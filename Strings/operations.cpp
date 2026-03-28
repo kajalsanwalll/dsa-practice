0b420d356ef19bc31425f51d6bd6ce6d1d33bcf8
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-28 13:49:35 +0530

    toggle case

--- Strings/operations.cpp
+++ Strings/operations.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 char S[] = "WELCOME";
+char S1[] = "wElCome";
 
 int main(){
 
@@ -16,5 +17,19 @@ int main(){
         S[i] = S[i] + 32; //changing to lowercase
     }
     cout << "changed case: " << S << endl;
+
+    //toggle the case
+    cout << "current case: " << S1 << endl;
+    for(i=0; S1[i] != '\0';i++){
+
+        if(S1[i] >= 65 && S1[i]<=90){
+            S1[i]+=32;
+        }
+        else if(S1[i]>= 'a' && S1[i]<='z'){
+            S1[i]-=32;
+        }
+    }
+    cout << "toggled case: " << S1 << endl;
+
     return 0;
 }
