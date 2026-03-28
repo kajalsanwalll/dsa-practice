a7396ce216d98dbc9a8b29b8adc08b76fe86e739
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-28 13:15:56 +0530

    length of a string

---
+++ Strings/operations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+char S[] = "Welcome";
+
+int main(){
+
+    int i;
+    for(i=0; S[i] != '\0'; i++){
+
+    }
+    cout << "length of string is: " << i << endl; // length = i because \0 is not counted
+
+    return 0;
+}
