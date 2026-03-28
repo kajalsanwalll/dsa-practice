95f0572f68698cb366e0f8fd087c810328f730ad
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-28 14:08:47 +0530

    counting vowels and consonants

--- Strings/operations.cpp
+++ Strings/operations.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 char S[] = "WELCOME";
 char S1[] = "wElCome";
+char A[] = "How are you";
 
 int main(){
 
@@ -30,6 +31,22 @@ int main(){
         }
     }
     cout << "toggled case: " << S1 << endl;
+
+    //counting vowels and consonants
+    int vcount=0,ccount=0;
+
+    for(i=0; A[i]!= '\0';i++){
+
+        if(A[i] == 'a' || A[i] == 'e'|| A[i] == 'i' || A[i] == 'o' || A[i] == 'u' 
+            || A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U'){
+            vcount++;
+        }
+        else if((A[i]>=65 && A[i]<=90)|| (A[i]>=97 && A[i]<=122)){
+            ccount++;
+        }
+    }
+    cout << "no. of vowels: " << vcount << endl;
+    cout << "no. of consonants: " << ccount << endl;
 
     return 0;
 }
