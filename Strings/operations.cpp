6a49684405ed857038cde3f4603594ccd8af16cb
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-28 13:27:57 +0530

    change case

--- Strings/operations.cpp
+++ Strings/operations.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 using namespace std;
 
-char S[] = "Welcome";
+char S[] = "WELCOME";
 
 int main(){
 
+    // for length of string
     int i;
-    for(i=0; S[i] != '\0'; i++){
-
-    }
+    for(i=0; S[i] != '\0'; i++){}
     cout << "length of string is: " << i << endl; // length = i because \0 is not counted
 
+    //for changing the case 
+    cout << "original case: " << S << endl;
+    for(i=0; S[i]!= '\0'; i++){
+        S[i] = S[i] + 32; //changing to lowercase
+    }
+    cout << "changed case: " << S << endl;
     return 0;
 }
