5c22a2b9e478756d611726f9c556423350c9e242
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-29 21:21:31 +0530

    upper triangular matrix

---
+++ Matrices/upperTri.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Matrix{
+
+    int *A;
+    int n;
+
+};
+
+void set(struct Matrix *m, int i, int j, int n){
+
+    if(i==j){
+        
+    }
+
+}
+
+int main(){
+
+
+
+    return 0;
+}
