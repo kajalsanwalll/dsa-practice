0159a99413d1757bf01dbc3b8d14a35e6bd6ba89
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-29 11:26:34 +0530

    diagonal matrix class and scoap resolution functions

--- Matrices/diagonal.cpp
+++ Matrices/diagonal.cpp
@@ -45,6 +45,66 @@ void Display(struct Matrix m){
     }
 }
 
+
+// class for diagonal matrix
+class Diagonal{
+
+    private:
+        int n;
+        int *A;
+
+    public:
+        Diagonal(int n){
+            this->n=n;
+            A= new int[n]; // dynamic heap array
+        }
+        void set(int i, int j, int x);
+        int get(int i, int j);
+        void Display();
+        ~Diagonal(){
+            delete []A;
+
+        } 
+};
+
+void Diagonal::set(int i, int j , int x){
+
+    if(i==j){
+        A[i-1]=x;
+    }
+}
+
+int Diagonal ::get(int i, int j){
+
+    if(i==j){
+
+        return A[i-1];
+    }
+    else{
+        return 0;
+    }
+}
+
+void Diagonal::Display(){
+
+    int i,j;
+
+    for(i=0;i<n;i++){
+
+        for(j=0;j<n;j++){
+
+            if(i==j){
+                cout << " " << A[i];
+
+            }
+            else{
+                cout << " 0 ";
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main(){
 
     struct Matrix m;
