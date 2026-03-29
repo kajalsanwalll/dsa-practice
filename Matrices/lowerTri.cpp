ed6fa8767c27c91938714b14b50082af1dee8384
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-03-29 12:54:47 +0530

    class for Lower triangular matrix

--- Matrices/lowerTri.cpp
+++ Matrices/lowerTri.cpp
@@ -50,6 +50,73 @@ void Display(struct Matrix m){
     }
 }
 
+// class for lower triangular matrix
+class LowerTriangle{
+
+    private:
+        int n;
+        int *A;
+
+    public:
+        LowerTriangle(){
+          n=2; // minimum of 2 if not provided
+          A = new int[2*(2+1)/2];
+        }
+        LowerTriangle(int n){
+            this->n=n;
+            A= new int[n*(n+1)/2]; // dynamic heap array
+        }
+        void set(int i, int j, int x);
+        int get(int i, int j);
+        void Display();
+        ~LowerTriangle(){
+            delete []A;
+
+        } 
+};
+
+void LowerTriangle::set(int i, int j , int x){
+
+    if(i>=j){
+
+        A[i*(i-1)/2 + j-1] = x; // row major formula
+
+    }
+}
+
+int LowerTriangle ::get(int i, int j){
+
+    if(i>=j){
+
+        return A[i*(i-1)/2 + j-1];
+    }
+    else{
+
+        return 0;
+    }
+}
+
+void LowerTriangle::Display(){
+
+    int i,j;
+
+    for(i=1;i<=n;i++){
+
+        for(j=1;j<=n;j++){
+
+            if(i>=j){
+                cout << " "<< A[i*(i-1)/2 + j-1] ;
+            }
+            else{
+                cout << " 0 " ;
+            }
+            
+        }
+        cout << endl;
+    }
+}
+
+
 
 int main(){
 
