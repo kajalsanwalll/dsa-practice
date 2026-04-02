d505e15b5ba276bdc5718f866ecf60a3b927ae70
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-02 14:14:22 +0530

    poly representation

--- Matrices/Polynomial.cpp
+++ Matrices/Polynomial.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+struct Term{
+
+    int coeff;
+    int Exp;
+
+};
+
+struct Poly{
+
+    int n;
+    struct Term *terms;
+
+};
+
+void create(struct Poly *p){
+
+    int i;
+
+    cout << "Number of terms?" ;
+    cin >> p->n ;
+    cout << endl;
+
+    p->terms = new struct Term[p->n];
+
+    cout << "Enter terms: " << endl;
+    for(i=0;i<p->n;i++){
+        
+    }
+}
 
 
 int main(){
