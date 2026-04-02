1da0132bfcb7ba28d273ad4f014b6f5028054b84
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-02 14:22:45 +0530

    polynomial representation

--- Matrices/Polynomial.cpp
+++ Matrices/Polynomial.cpp
@@ -27,13 +27,28 @@ void create(struct Poly *p){
 
     cout << "Enter terms: " << endl;
     for(i=0;i<p->n;i++){
-        
+
+        cin >> p->terms[i].coeff;
+        cin >> p->terms[i].Exp;
     }
+}
+
+void Display(struct Poly p){
+
+    int i;
+    for(i=0; i< p.n; i++){
+
+        cout << p.terms[i].coeff << "x" << p.terms[i].Exp << " + ";
+    }
+    cout << endl;
 }
 
 
 int main(){
 
+    struct Poly p1;
+    create(&p1);
+    Display(p1);
 
     return 0;
 }
