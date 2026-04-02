9b205ac474e62c9ff6ab329a20b5110924cb04ce
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-02 11:56:08 +0530

    sparse matric cpp class

--- Matrices/sparseCppClass.cpp
+++ Matrices/sparseCppClass.cpp
@@ -24,5 +24,46 @@ class Sparse{
             this->m = m;
             this->n = n;
             this->num = num;
+            e= new Element[this->num];
          }     
+
+         ~Sparse(){
+
+            delete [] e;
+         }
+
+         void read(){
+
+            cout << "Enter Non- zero elements: " << endl;
+            int i;
+            for(i=0;i<num;i++){
+                cin >> e[i].i >> e[i].j >> e[i].x;
+
+            }
+         }
+         void display(){
+            int i,j,k=0;
+
+            for(i=0;i<m;i++){
+
+                for(j=0;j<n;j++){
+
+                    if(e[k].i==i && e[k].j==j){
+
+                        cout << e[k++].x << " ";
+                    }
+                    else{
+                        cout << "0 ";
+                    }
+                }
+                cout << endl;
+            }
+         }
 };
+
+int main(){
+
+    Sparse s1(5,5,5);
+
+    return 0;
+}
