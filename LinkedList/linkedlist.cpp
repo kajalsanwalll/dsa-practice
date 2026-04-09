dd9147a614b8a4fcc9aad48eba1014d9e4f13262
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-09 12:13:23 +0530

    insert node in linked list

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -133,6 +133,31 @@ struct Node * LSearch(struct Node *p, int key){
     return NULL;
 }
 
+void Insert(struct Node *p,int pos, int x){
+
+    struct Node *t, *p;
+    if(pos == 0){
+        t = new Node;
+        t->data = x;
+        t->next = first;
+        first= t;
+    }
+    else if(pos > 0){
+
+        p= first;
+        for(int i=0; i< pos-1 && p;i++){
+            p= p->next;
+        }
+
+        if(p){
+            t= new Node;
+            t->data = x;
+            t->next = p->next;
+            p->next = t;
+        }
+    }
+}
+
 int main(){
 
     struct Node *temp;
