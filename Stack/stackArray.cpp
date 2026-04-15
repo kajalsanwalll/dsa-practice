aa019ab1b4c4b06887a1463c2c33a91886e9475c
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-15 17:20:06 +0530

    Stack

---
+++ Stack/stackArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Stack{
+
+    int size;
+    int Top;
+    int *s;
+
+};
+
+void create(struct Stack *st){
+
+    
+}
+
+
+int main(){
+
+
+    return 0;
+}
