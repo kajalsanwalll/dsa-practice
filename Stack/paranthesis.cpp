5629c0a5ac3b779e7887f50a13c75984676b152c
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-15 22:21:57 +0530

    paranthesis using stack

---
+++ Stack/paranthesis.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+
+    char data;
+    struct Node *next;
+}*top = NULL;
+
+void push(char x){
+
+    struct Node *t;
+    t = new Node;
+
+    if(t== NULL){
+
+        cout << "stack is full" << endl;
+    }
+    else{
+
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+
+char pop(){
+
+    struct Node *t;
+    char x = -1;
+
+    if(top == NULL){
+        cout << "stack is empty!" << endl;
+    }
+    else{
+
+        t = top;
+        top = top->next;
+        x = t->data;
+        delete t;   
+
+    }
+    return x;
+}
+
+void display(){
+
+    struct Node *p;
+    p = top;
+
+    while(p != NULL){
+
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int isBalanced(char *exp){
+
+    int i;
+
+    for(i=0; exp[i] != '\0'; i++){
+
+        if(exp[i] == '('){
+            push(exp[i]);
+        }
+    }
+
+}
+
+int main(){
+
+    char *exp= "((a+b) * (c+d))";
+
+    return 0;
+}
