a4b4caa65f683ac6d79b1068c9a02a025f91c873
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-16 10:02:17 +0530

    infix to postfix using stack function added

--- Stack/infixToPostfix.cpp
+++ Stack/infixToPostfix.cpp
@@ -5,7 +5,7 @@ struct Node{
 
     char data;
     struct Node *next;  
-} *top = NULL;
+} *top;
 
 void push(char x){
 
@@ -70,17 +70,54 @@ int pre(char x){
 int isOperand(char x){
 
     if(x == '+' || x == '-' || x == '*' || x == '/'){
-        return 1;
+        return 0;
     }
-    return 0;
+    return 1;
 
+}
+
+char * InfixToPostfix(const char *infix){
+
+    int i=0, j=0;
+    char *postfix;   
+    int len = strlen(infix);
+
+    postfix = new char[len + 2];
+
+    while(infix[i] != '\0'){
+
+        if(isOperand(infix[i])){
+
+            postfix[j++] = infix[i++];
+        }
+        else{
+
+            if(pre(infix[i]) > pre(top->data)){
+
+                push(infix[i++]);
+            }
+            else{
+
+                postfix[j++] = pop();
+            }
+        }
+    }
+
+    while(top != NULL){
+
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+    return postfix;
 }
 
 int main(){
 
-    char *infix = "a+b*c";
+    const char *infix = "a+b*c-d/e";
+    push('#');
 
-    char *postfix;
+    char *postfix = InfixToPostfix(infix);
+    cout << postfix << endl;
 
     return 0;
 }
