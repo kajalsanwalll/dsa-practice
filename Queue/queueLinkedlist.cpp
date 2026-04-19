0b09320653679308b2675fa3a1df7ee327c1cc27
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-19 15:51:04 +0530

    enqueue function for queue using linked list

--- Queue/queueLinkedlist.cpp
+++ Queue/queueLinkedlist.cpp
@@ -7,6 +7,28 @@ struct Node{
     struct Node *next;
 }*front = NULL, *rear = NULL;
 
+void enqueue(int x){
+
+    struct Node *t;
+    t = new Node;
+
+    if(t == NULL){
+        cout << "queue is full";
+    }
+    else{
+
+        t->data =x;
+        t->next = NULL;
+        if(front==NULL){
+            front=rear = t;
+        }else{
+
+            rear->next = t;
+            rear = t;
+        }
+    }
+}
+
 
 int main(){
 
