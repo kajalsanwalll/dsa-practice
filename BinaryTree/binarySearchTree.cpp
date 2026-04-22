034cbf9b3220dbfa9cce43d858191e56302e15ea
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-22 16:49:09 +0530

    creation of binary search tree

--- BinaryTree/binarySearchTree.cpp
+++ BinaryTree/binarySearchTree.cpp
@@ -1,8 +1,56 @@
 #include <iostream>
 using namespace std;
 
+struct Node{
+
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+
+} *root = NULL;
 
 
+void Insert(int key){
+
+    struct Node *t = root;
+    struct Node *r; // tailing pointer
+    struct Node *p; //to make new nodes
+
+    if(root == NULL){
+
+        p = new Node;
+        p->data = key;
+        p->lchild = p->rchild = NULL;
+        return;
+    }
+    while(t != NULL){
+
+        r = t;
+        if(key < t->data){
+            t = t->lchild;
+        }
+        else if(key > t->data){
+            t = t->rchild;
+        }
+        else {
+            return;
+        }
+    }
+
+    p = new Node;
+    p->data = key;
+    p->lchild = p->rchild = NULL;
+
+    if(key < r->data){
+
+        r->lchild = p;
+    }
+    else{
+
+        r->rchild = p;
+    }
+
+}
 
 int main(){
 
