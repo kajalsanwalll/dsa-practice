cce49795252ad994520e7a4c16389158a9f5dd85
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-23 18:22:16 +0530

    AVL trees

---
+++ AVL Trees/avl.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    struct Node *lchild;
+    int data;
+    int height;
+    struct Node *rchild;
+}*root = NULL;
+
+int main(){
+
+
+
+    return 0;
+}
