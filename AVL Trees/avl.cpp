4574a70d2b38e2058be54d7dd1ef17dbc8f7aa3a
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-23 18:33:09 +0530

    insertion of nodes

--- AVL Trees/avl.cpp
+++ AVL Trees/avl.cpp
@@ -8,6 +8,32 @@ struct Node{
     struct Node *rchild;
 }*root = NULL;
 
+//recursive insertion of avl trees
+struct Node *RInsert(struct Node *p, int key){
+
+    struct Node *t=NULL;
+
+    if(p == NULL){
+
+        t = new Node;
+        t->data = key;
+        t->height = 1;
+        t->lchild = t->rchild = NULL;
+        return t;
+    }
+
+    if(key < p->data){
+        p->lchild = RInsert(p->lchild, key);
+    }
+    else if(key > p->data){
+        p->rchild = RInsert(p->rchild, key);
+    }
+
+    p->height = NodeHeight();
+    return p;
+}
+
+
 int main(){
 
 
