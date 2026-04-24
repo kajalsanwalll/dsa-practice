8cc6cd45e0746d5561eff2bcf386d6ee3c94e12d
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-24 16:45:15 +0530

    delete function for heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -16,6 +16,37 @@ void Insert(int H[], int n){
 
 }
 
+int Delete(int H[], int n){
+
+    int i,j,x;
+    int temp,val;
+    val = H[1];
+
+    x = H[n]; // we can delete first element only
+    H[1] = H[n];
+    H[n] = val;
+    i=1;j=i*2;
+
+    while(j<n-1){
+
+        if(H[j+1] > H[j]){
+            j = j+1;
+        }
+
+        if(H[i]<H[j]){
+            temp = H[i];
+            H[i] = H[j];
+            H[j] = temp;
+            i=j;
+            j=2*j;
+        }
+        else{
+            break;
+        }
+    }
+    return val; //deleted value
+}
+
 
 int main(){
 
@@ -26,6 +57,8 @@ int main(){
     for(i=2;i<=7;i++){
         Insert(H,i);
     }
+
+    cout << "deleted value is: " << Delete(H,7) << endl;
 
     for(i=1;i<=7;i++){
         cout << H[i] << " ";
