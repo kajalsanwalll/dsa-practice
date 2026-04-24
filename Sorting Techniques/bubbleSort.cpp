2acc58d0d24f2e69bf1d472591e56e01131e891c
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-24 19:03:00 +0530

    bubble sort function

--- Sorting Techniques/bubbleSort.cpp
+++ Sorting Techniques/bubbleSort.cpp
@@ -22,8 +22,14 @@ void bubbleSort(int A[], int n){
 
 int main(){
 
+    int i;
     int A[] = {3,7,9,10,6,5,12,4,11,2}, n = 10;
     bubbleSort(A,10);
+
+    for(i=0;i<10;i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
