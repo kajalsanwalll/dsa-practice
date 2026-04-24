cc7e0f6b994c5e0c09db9ba44b066de90b78292c
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-24 16:17:28 +0530

    insert function for heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 using namespace std;
 
+//insert in a heap
+void Insert(int H[], int n){
 
+    int i=n; int temp;
+    temp = H[i];
+
+    while (i>1 && temp > H[i/2])
+    {
+        H[i] = H[i/2];
+        i = i/2;
+    }
+    H[i] = temp;
+
+}
 
 
 int main(){
 
 
+    int H[] = {0,2,5,8,9,4,10,7};
 
     return 0;
 }
