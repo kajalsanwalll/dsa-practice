84707df69942f162f0ea99a7f26abf12887b87e1
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-24 16:24:31 +0530

    display elements in heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -19,8 +19,18 @@ void Insert(int H[], int n){
 
 int main(){
 
+    int H[] = {0,10,20,30,25,5,40,35};
+    //result in a max heap should be 40,25,35,10,5,20,30
+    int i;
 
-    int H[] = {0,2,5,8,9,4,10,7};
+    for(i=2;i<=7;i++){
+        Insert(H,i);
+    }
+
+    for(i=1;i<=7;i++){
+        cout << H[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
