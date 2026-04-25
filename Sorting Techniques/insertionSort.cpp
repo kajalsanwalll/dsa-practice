02d8cad4b0b0f1147edb296d5f2edb9fb1c4c27b
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 11:05:23 +0530

    insertion sort function

--- Sorting Techniques/insertionSort.cpp
+++ Sorting Techniques/insertionSort.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void insertionSort(int A[], int n){
 
+    int i,j,x;
+
+    for(i=1;i<n;i++){
+
+        j = i-1;
+        x = A[i];
+
+        while (j > -1 && A[j]>x)
+        {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = x;
+        
+    }
+
+}
 
 
 int main(){
@@ -9,6 +27,9 @@ int main(){
     int i;
     int A[] = {3,7,9,10,6,5,12,4,11,2}, n = 10;
     
+    for(i=1;i<n;i++){
+        insertionSort(A,10);
+    }
 
     for(i=0;i<10;i++){
         cout << A[i] << " ";
