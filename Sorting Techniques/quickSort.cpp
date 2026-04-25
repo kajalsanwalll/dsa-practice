d13148618decdd01df496e262b214e7e9c734070
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 12:28:32 +0530

    quick sort started

---
+++ Sorting Techniques/quickSort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void quickSort(int A[], int n){
+
+    int i;
+
+}
+
+int main(){
+
+    int i;
+    int A[] = {3,7,9,10,6,5,12,4,11,2}, n = 10;
+    quickSort(A,n);
+
+    for(i=0;i<10;i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
