49e30bf9f3f2b69d8a28d865b7ae4b52dd2a16e0
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 16:39:47 +0530

    merge sort using iteration

--- Sorting Techniques/mergeSort.cpp
+++ Sorting Techniques/mergeSort.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 //merge basic function
-void Merge(int A[], int l, int h, int mid){
+void Merge(int A[], int l, int mid, int h){
 
-    int i,j,k;
+    int i=l,j=mid+1,k=l;
     int B[100];
 
     while(i<= mid && j<=h){
@@ -29,17 +29,30 @@ void Merge(int A[], int l, int h, int mid){
 }
 
 //merge sort iterative function
-void IMergeSort(int A[], int l, int h){
+void IMergeSort(int A[], int n){
 
-    int j;
-    
+    int p,l,h,mid,i;
+
+    for(p =2;p <= n;p=p*2){
+
+        for(i=0;i+p-1<n;i=i+p){
+
+            l=i;
+            h = i+p-1;
+            mid =(l+h)/2;
+            Merge(A,l,mid,h);
+        }
+    }
+    if(p/2 < n){
+        Merge(A,0,p/2-1,n-1);
+    }
 
 }
 
 int main(){
 
     int A[] = {11,13,7,12,16,9,24,5,10,3,INT32_MAX}, n = 11,i;
-    IMergeSort(A,0,n-1);
+    IMergeSort(A,n);
 
     for(i=0;i<10;i++){
         cout << A[i] << " ";
