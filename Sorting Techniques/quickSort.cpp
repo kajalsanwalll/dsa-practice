f27824c01d8bf285bcfaef2020e2a07d61635000
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 13:59:29 +0530

    partition function for quick sort

--- Sorting Techniques/quickSort.cpp
+++ Sorting Techniques/quickSort.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void quickSort(int A[], int n){
+//partition function for quick sort
+int Partition(int A[], int l, int h){
 
-    int i;
+    int pivot = A[l];
+    int i=l,j=h;
 
+    do{
+
+        do{i++;}while(A[i]<= pivot);
+        do{j--;}while(A[j]>pivot);
+
+        if(i<j){
+            swap(A[i],A[j]);
+        }
+
+    }while(i<j);
+    swap(A[pivot],A[j]);
+
+    return j;
+}
+
+//quick sort recursive function
+void QuickSort(int A[], int n){
+
+    
 }
 
 int main(){
 
     int i;
-    int A[] = {3,7,9,10,6,5,12,4,11,2}, n = 10;
-    quickSort(A,n);
+    int A[] = {11,13,7,12,16,9,24,5,10,3,INT_MAX}, n = 11;
+    Partition(A,11,3);
 
     for(i=0;i<10;i++){
         cout << A[i] << " ";
