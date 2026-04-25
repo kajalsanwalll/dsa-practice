69de99bceb249a17b1470dea14f62123743578e3
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 09:44:09 +0530

    insertion sort start

---
+++ Sorting Techniques/insertionSort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+
+    int i;
+    int A[] = {3,7,9,10,6,5,12,4,11,2}, n = 10;
+    
+
+    for(i=0;i<10;i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
