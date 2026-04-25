d778a063a26f94312a21f116846e081cc071ad97
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 17:43:25 +0530

    count sort started

---
+++ Sorting Techniques/count.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int A[] = {11,13,7,12,16,9,24,5,10,3,INT32_MAX}, n = 11,i;
+    
+
+    //display 
+    for(i=0;i<10;i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
