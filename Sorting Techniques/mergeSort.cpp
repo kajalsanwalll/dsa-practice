d7b4c36284036578773950d67da22b34b28f079d
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-25 14:15:07 +0530

    merge sort started

---
+++ Sorting Techniques/mergeSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+//quick sort recursive function
+void mergeSort(int A[], int l, int h){
+
+    int j;
+    
+
+}
+
+int main(){
+
+    int A[] = {11,13,7,12,16,9,24,5,10,3,INT32_MAX}, n = 11,i;
+    mergeSort(A,0,n-1);
+
+    for(i=0;i<10;i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
