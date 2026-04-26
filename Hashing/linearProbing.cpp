31159380838f3ab3469f6e9f1b78e56b25d7bec0
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-04-26 17:14:44 +0530

    search function for linear probing

--- Hashing/linearProbing.cpp
+++ Hashing/linearProbing.cpp
@@ -28,16 +28,27 @@ void Insert(int H[], int key){
 
 }
 
+int Search(int H[], int key){
+    int index = Hash(key);
 
+    int i=0;
+
+    while(H[(index+i)%SIZE]!= key){
+        i++;
+    }
+    return (index+i)%SIZE;
+}
 
 int main(){
 
-    int HT[10];
+    int HT[10] ={0};
 
     Insert(HT,12);
     Insert(HT,4);
     Insert(HT,9);
     Insert(HT,14);
+
+    cout << "key found at " << Search(HT,14) << endl;
 
     return 0;
 }
